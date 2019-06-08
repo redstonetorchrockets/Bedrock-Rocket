fe9d70a9fa45c63d568b8fe3be020e52eeca410c
konrad2002 <konrad@schwimmteamerzgebirge.de>
2019-06-08 13:50:56 +0200

    fixed issues / added client script

--- scripts/client/client.js
+++ scripts/client/client.js
@@ -1,14 +1,48 @@
-var system = client.registerSystem(0, 0);
+var ClientSystem = client.registerSystem(0, 0);
 
-system.initialize = function() {
-    this.listenForEvent("minecraft:entity_created", (eventData) => this.onEntityDeath(eventData));
+ClientSystem.initialize = function() {
+    this.listenForEvent("minecraft:ui_event", (eventData) => this.onUIMessage(eventData));
+    this.listenForEvent("minecraft:client_entered_world", (eventData) => this.onClientEnteredWorld(eventData));
+    this.listenForEvent("minecraft:hit_result_changed", (eventData) => this.onPick(eventData));
 };
 
-system.onEntityDeath = function(eventData) {
-    this.broadcastEvent("minecraft:display_chat_event", "Entity detected");
-    if (eventData.entity == "rtr:rocket") {
-        this.broadcastEvent("minecraft:display_chat_event", "Rocket detected!");
+ClientSystem.update = function() {};
+
+ClientSystem.onClientEnteredWorld = function(eventData) {
+    // Client has entered the world, show the starting screen
+    let loadEventData = this.createEventData("minecraft:load_ui");
+    loadEventData.data.path = "test.html";
+    loadEventData.data.options.is_showing_menu = false;
+    loadEventData.data.options.absorbs_input = true;
+    ClientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
+
+};
+
+ClientSystem.onUIMessage = function(eventDataObject) {
+    let eventData = eventDataObject.data;
+    if (eventData == "leavePressed") {
+        this.broadcastEvent("minecraft:display_chat_event", "§eAddon by §cRedstoneTorchRockets§9 Visit §brtr.logilutions.de§r!");
+        let unloadEventData = this.createEventData("minecraft:unload_ui");
+        unloadEventData.data.path = "test.html";
+        this.broadcastEvent("minecraft:unload_ui", unloadEventData);
+        this.sendMsg("§eAddon by §cRedstoneTorchRockets§9 Visit §brtr.logilutions.de§r!");
+    } else if (eventData == "visitWebsite") {
+        this.location.href = "http://rtr.logilutions.de";
     }
 };
 
-system.update = function() {};
+ClientSystem.runCommand = function(command) {
+    commandData = this.createEventData("minecraft:execute_command")
+    commandData.data.command = command
+    this.broadcastEvent("minecraft:execute_command", commandData)
+}
+
+ClientSystem.sendMsg = function(msg) {
+    this.broadcastEvent("minecraft:display_chat_event", msg);
+}
+
+ClientSystem.onPick = function(eventData) {
+    if (eventData.position !== null) {
+        this.broadcastEvent("minecraft:display_chat_event", "Pick at x:" + eventData.position.x + " y:" + eventData.position.y + " z:" + eventData.position.z);
+    }
+};
--- scripts/server/server.js
+++ scripts/server/server.js
@@ -1,20 +1,19 @@
-var system = server.registerSystem(0, 0);
+var serverSystem = server.registerSystem(0, 0);
 
-system.initialize = function() {
+serverSystem.initialize = function() {
     //minecraft:player_attacked_entity 
     this.listenForEvent("minecraft:player_attacked_entity", (eventData) => this.onPick(eventData));
 };
 
-system.onPick = function(eventData) {
-    this.runCommand("execute @p ~ ~ ~ esay §eDas tut weh @" + eventData.data.player.__type__ + "!§r");
-    this.runCommand("w @p Das macht man nicht!");
+serverSystem.onPick = function(eventData) {
+    this.runCommand("say §e @p ,§r das macht man nicht!");
 };
 
-system.update = function() {
+serverSystem.update = function() {
     this.runCommand("function rocketFlight");
 }
 
-system.runCommand = function(command) {
+serverSystem.runCommand = function(command) {
     commandData = this.createEventData("minecraft:execute_command")
     commandData.data.command = command
     this.broadcastEvent("minecraft:execute_command", commandData)
