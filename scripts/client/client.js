b499032adb55d793caa5c46b8196de4b8c6abdb3
konrad2002 <konrad@schwimmteamerzgebirge.de>
2019-06-17 21:38:12 +0200

    ADDED WORKING GUI!!!

--- scripts/client/client.js
+++ scripts/client/client.js
@@ -1,39 +1,40 @@
-var ClientSystem = client.registerSystem(0, 0);
+var clientSystem = client.registerSystem(0, 0);
 
 
 let globalVars = {};
-globalVars.lookingAtRocket = false;
+globalVars.rocket = false;
 globalVars.position = null;
 
-ClientSystem.initialize = function() {
+clientSystem.initialize = function() {
     this.listenForEvent("minecraft:ui_event", (eventData) => this.onUIMessage(eventData));
     this.listenForEvent("minecraft:client_entered_world", (eventData) => this.onClientEnteredWorld(eventData));
     this.listenForEvent("minecraft:hit_result_continuous", (eventData) => this.onLookingAt(eventData));
+    this.listenForEvent("rtr:entered_rocket", (eventData) => this.onRocket(eventData));
+    this.listenForEvent("rtr:left_rocket", (eventData) => this.onLeftRocket(eventData));
 };
 
-ClientSystem.update = function() {};
+clientSystem.update = function() {};
 
-ClientSystem.onClientEnteredWorld = function(eventData) {
+clientSystem.onClientEnteredWorld = function(eventData) {
     // Client has entered the world, show the starting screen
     let loadEventData = this.createEventData("minecraft:load_ui");
     loadEventData.data.path = "test.html";
     loadEventData.data.options.is_showing_menu = false;
     loadEventData.data.options.absorbs_input = true;
-    ClientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
+    clientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
 
 };
 
-ClientSystem.onUIMessage = function(eventDataObject) {
+clientSystem.onUIMessage = function(eventDataObject) {
     let eventData = eventDataObject.data;
     if (eventData == "leavePressed") {
-        this.broadcastEvent("minecraft:display_chat_event", "§eAddon by §cRedstoneTorchRockets§9 Visit §brtr.logilutions.de§r!");
         let unloadEventData = this.createEventData("minecraft:unload_ui");
         unloadEventData.data.path = "test.html";
         this.broadcastEvent("minecraft:unload_ui", unloadEventData);
+        this.sendMsg("§eAddon by §cRedstoneTorchRockets§9 Visit §brtr.logilutions.de§r!");
     } else if (eventData == "leaveRocket") {
-        this.broadcastEvent("minecraft:display_chat_event", "§eAddon by §cRedstoneTorchRockets§9 Visit §brtr.logilutions.de§r!");
         let unloadEventData = this.createEventData("minecraft:unload_ui");
-        unloadEventData.data.path = "lookingAtRocket.html";
+        unloadEventData.data.path = "cockpit.html";
         this.broadcastEvent("minecraft:unload_ui", unloadEventData);
     } else if (eventData == "readyForRocket") {
         globalVars.lookingAtRocket = false;
@@ -42,23 +43,43 @@ ClientSystem.onUIMessage = function(eventDataObject) {
     }
 };
 
-ClientSystem.runCommand = function(command) {
+clientSystem.runCommand = function(command) {
     commandData = this.createEventData("minecraft:execute_command")
     commandData.data.command = command
     this.broadcastEvent("minecraft:execute_command", commandData)
 }
 
-ClientSystem.sendMsg = function(msg) {
-    this.broadcastEvent("minecraft:display_chat_event", msg);
+clientSystem.sendMsg = function(msg) {
+    let message = this.createEventData("minecraft:display_chat_event");
+    message.data.message = msg;
+    clientSystem.broadcastEvent("minecraft:display_chat_event", message);
 }
 
-ClientSystem.onPick = function(eventData) {
-    if (eventData.position !== null) {
-        this.broadcastEvent("minecraft:display_chat_event", "Pick at x:" + eventData.position.x + " y:" + eventData.position.y + " z:" + eventData.position.z);
+clientSystem.onRocket = function(eventData) {
+    if (!globalVars.rocket) {
+        this.sendMsg("§eLoading GUI...§r");
+        let loadEventData = this.createEventData("minecraft:load_ui");
+        loadEventData.data.path = "cockpit.html";
+        loadEventData.data.options.is_showing_menu = false;
+        loadEventData.data.options.absorbs_input = false;
+        loadEventData.data.options.should_steal_mouse = true;
+        loadEventData.data.options.render_game_behind = true;
+        clientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
+        globalVars.rocket = false;
     }
-};
+}
 
-ClientSystem.onLookingAt = function(eventData) {
+clientSystem.onLeftRocket = function(eventData) {
+    if (globalVars.rocket) {
+        this.sendMsg("§eUnloading GUI...§r");
+        let unloadEventData = this.createEventData("minecraft:unload_ui");
+        unloadEventData.data.path = "cockpit.html";
+        this.broadcastEvent("minecraft:unload_ui", unloadEventData);
+        globalVars.rocket = true;
+    }
+}
+
+clientSystem.onLookingAt = function(eventData) {
     if (eventData.position !== null && eventData.data.entity.__identifier__ == "rtr:rocket" && globalVars.lookingAtRocket) {
         let loadEventData = this.createEventData("minecraft:load_ui");
         loadEventData.data.path = "lookingAtRocket.html";
@@ -66,7 +87,7 @@ ClientSystem.onLookingAt = function(eventData) {
         loadEventData.data.options.absorbs_input = false;
         loadEventData.data.options.should_steal_mouse = true;
         loadEventData.data.options.render_game_behind = true;
-        ClientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
+        clientSystem.broadcastEvent("minecraft:load_ui", loadEventData);
     } else {
         if (eventData.position == null || eventData.data.entity.__identifier__ !== "rtr:rocket") {
             let unloadEventData = this.createEventData("minecraft:unload_ui");
--- scripts/server/server.js
+++ scripts/server/server.js
@@ -4,6 +4,9 @@ serverSystem.initialize = function() {
     //minecraft:player_attacked_entity 
     this.listenForEvent("minecraft:player_attacked_entity", (eventData) => this.onPick(eventData));
     this.listenForEvent("minecraft:entity_start_riding", (eventData) => this.onRiding(eventData));
+    this.listenForEvent("minecraft:entity_stop_riding", (eventData) => this.onStopRiding(eventData));
+    this.registerEventData("rtr:entered_rocket", {});
+    this.registerEventData("rtr:left_rocket", {});
 };
 
 serverSystem.onPick = function(eventData) {
@@ -11,7 +14,15 @@ serverSystem.onPick = function(eventData) {
 };
 
 serverSystem.onRiding = function(eventData) {
-    this.runCommand("say §aRiding!§r");
+    if (eventData.data.ride.__identifier__ == "rtr:rocket") {
+        let enteredRocketData = this.createEventData("rtr:entered_rocket");
+        this.broadcastEvent("rtr:entered_rocket", enteredRocketData);
+    }
+}
+
+serverSystem.onStopRiding = function(eventData) {
+    let enteredRocketData = this.createEventData("rtr:left_rocket");
+    this.broadcastEvent("rtr:left_rocket", enteredRocketData);
 }
 
 serverSystem.update = function() {
